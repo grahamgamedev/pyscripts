3c6efac6410a198055386d9c7f35cfd13944ec75
grahamgamedev <grahamwaters2005@gmail.com>
2019-08-28 17:38:14 +0200

    first commit

---
+++ string test.py
@@ -0,0 +1,36 @@
+print("this is a string.")
+print('this is also a string.')
+print("strings " + "can " "be " + "added.")
+print("strings can be multaplyed. " * 3)
+
+print("\n is line brake.")
+print("\\ is backslash.")
+print("\r is return.")
+print("\b is backspace")
+
+print("")
+print("""this
+is
+a
+big
+string.""")
+
+print("")
+test = "len finds the lenth of a string."
+print(test)
+print(len(test))
+
+#Input makes a string
+print("")
+test = input("enter text ")
+print(test + " " * 4)
+
+#And so dose str
+print("")
+number = 42
+test = str(number)
+print((test + " ") * 4)
+
+
+
+
